04614dfdf5b3cf7036e8d389138489b002065c1f
Aam Hermansyah <74630284+AamHermansyah@users.noreply.github.com>
2022-10-15 10:56:31 +0700

    Update index.js

--- components/Teams/index.js
+++ components/Teams/index.js
@@ -6,7 +6,7 @@ export default function Teams(){
         <section id="team" className="mt-8">
             <div className="p-8 bg-white dark:bg-gray-800 rounded-lg shadow">
                 <p className="text-center text-3xl font-bold text-gray-800 dark:text-white">Our Team</p>
-                <blockquote className="text-center mb-12 text-xl font-normal text-gray-500 dark:text-gray-200 italic">"Hal besar dimulai dari kecil"</blockquote>
+                <blockquote className="text-center mb-12 text-xl font-normal text-gray-500 dark:text-gray-200 italic">&ldquo;Hal besar dimulai dari kecil&rdquo;</blockquote>
                 <div className="flex items-center flex-col flex-wrap sm:flex-row justify-evenly">
                     {teams.map((data, index) => (
                         <Card data={data} key={index} />
