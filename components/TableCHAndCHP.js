5c309bdb6ae69da07789fef49f84e4cfdfa77396
Aam Hermansyah <amzhermanzyah@gmail.com>
2022-10-23 17:55:38 +0700

    fix tabel laba

--- components/TableCHAndCHP.js
+++ components/TableCHAndCHP.js
@@ -1,8 +1,8 @@
 export default function TablePercentagePerYears(){
     
     return (
-        <div className="flex flex-col items-center bg-white p-2 sm:p-4 rounded-sm sm:rounded-md" id="tabel-laba">
-            <h2 className="text-lg ss:text-2xl font-semibold">Tabel laba</h2>
+        <div className="flex flex-col justify-center items-center bg-white max-w-4xl p-4 sm:p-8 shadow-md rounded-md mx-auto" id="tabel-laba">
+            <h2 className="text-lg ss:text-2xl font-semibold mb-4">Tabel laba</h2>
             <table className="table w-full max-w-4xl shadow rounded-lg">
                 <thead>
                     <tr className="text-[.7rem] ss:text-base ss:font-semibold">
@@ -28,14 +28,28 @@ export default function TablePercentagePerYears(){
                         <td className="border-b-2 p-4 dark:border-dark-5">$543.21</td>
                     </tr>
                     <tr className="text-gray-700 odd:bg-gray-200">
-                        <td className="border-b-2 p-4 dark:border-dark-5">2022</td>
+                        <td className="border-b-2 p-4 dark:border-dark-5">2021</td>
                         <td className="border-b-2 p-4 dark:border-dark-5">
                             <div className={`${true >= 0 ? "text-green-500" : "text-red-600"} flex justify-center items-center gap-x-`}>
                                 <svg className={`${false < 0 ? 'rotate-180' : ''} w-2 h-2 ss:w-4 ss:h-4`} fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
                                     </path>
                                 </svg>
-                                <span>123%</span>
+                                <span>1.3%</span>
+                            </div>
+                        </td>
+                        <td className="border-b-2 p-4 dark:border-dark-5">$123.45</td>
+                        <td className="border-b-2 p-4 dark:border-dark-5">$543.21</td>
+                    </tr>
+                    <tr className="text-gray-700 odd:bg-gray-200">
+                        <td className="border-b-2 p-4 dark:border-dark-5">2020</td>
+                        <td className="border-b-2 p-4 dark:border-dark-5">
+                            <div className={`${true >= 0 ? "text-green-500" : "text-red-600"} flex justify-center items-center gap-x-`}>
+                                <svg className={`${false < 0 ? 'rotate-180' : ''} w-2 h-2 ss:w-4 ss:h-4`} fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1408 1216q0 26-19 45t-45 19h-896q-26 0-45-19t-19-45 19-45l448-448q19-19 45-19t45 19l448 448q19 19 19 45z">
+                                    </path>
+                                </svg>
+                                <span>12%</span>
                             </div>
                         </td>
                         <td className="border-b-2 p-4 dark:border-dark-5">$123.45</td>
