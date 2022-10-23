a925a4ed6290a8fc8f92e7523ab5cc7563f5ae3e
Aam Hermansyah <amzhermanzyah@gmail.com>
2022-10-23 13:56:01 +0700

    memperbaiki bug API

--- app/ProviderWraper.js
+++ app/ProviderWraper.js
@@ -5,10 +5,10 @@ import { addGoldDataPrice, addGoldPriceCurrent, setLoadingFetchDataGoldPrice } f
 import formatDate from "../utils/formatDate";
 import { CHP_IDEAL_FOR_BUY, CONFIG_DATE, SECRET_KEY, URL } from "../utils/config";
 import convertGoldDataToArray from "../promises/convertGoldDataToArray";
-import gold_price_one_year from "../constants/json/gold_price_one_year.json";
+// import gold_price_one_year from "../constants/json/gold_price_one_year.json";
 import { useEffect } from "react";
 import { countLossAndProfit, getMaxLowNumber } from "../utils/utilsForNumber";
-// import gold_price_annual from "../constants/json/gold_price_annual.json";
+import gold_price_annual from "../constants/json/gold_price_annual.json";
 import getDataHistory from "../promises/getDataHistory";
 
 export default function ProviderWraper({children}){
